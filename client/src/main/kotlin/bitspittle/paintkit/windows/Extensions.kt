52c8e7489a088994f4ab714030e03c5be5151223
David Herman <bit.spittle@gmail.com>
2021-04-10 18:29:22 -0700

    Update WindowEvents.plus to be less wasteful

--- client/src/main/kotlin/bitspittle/paintkit/windows/Extensions.kt
+++ client/src/main/kotlin/bitspittle/paintkit/windows/Extensions.kt
@@ -4,14 +4,32 @@ import androidx.compose.desktop.WindowEvents
 
 operator fun WindowEvents.plus(other: WindowEvents): WindowEvents {
     return WindowEvents(
-        onOpen = { this.onOpen?.invoke(); other.onOpen?.invoke() },
-        onClose = { this.onClose?.invoke(); other.onClose?.invoke() },
-        onMinimize = { this.onMinimize?.invoke(); other.onMinimize?.invoke() },
-        onMaximize = { this.onMaximize?.invoke(); other.onMaximize?.invoke() },
-        onRestore = { this.onRestore?.invoke(); other.onRestore?.invoke() },
-        onFocusGet = { this.onFocusGet?.invoke(); other.onFocusGet?.invoke() },
-        onFocusLost = { this.onFocusLost?.invoke(); other.onFocusLost?.invoke() },
-        onResize = { intSize -> this.onResize?.invoke(intSize); other.onResize?.invoke(intSize) },
-        onRelocate = { intOffset -> this.onRelocate?.invoke(intOffset); other.onRelocate?.invoke(intOffset) },
+        onOpen = if (this.onOpen != null || other.onOpen != null) {
+            { this.onOpen?.invoke(); other.onOpen?.invoke() }
+        } else null,
+        onClose = if (this.onClose != null || other.onClose != null) {
+            { this.onClose?.invoke(); other.onClose?.invoke() }
+        } else null,
+        onMinimize = if (this.onMinimize != null || other.onMinimize != null) {
+            { this.onMinimize?.invoke(); other.onMinimize?.invoke() }
+        } else null,
+        onMaximize = if (this.onMaximize != null || other.onMaximize != null) {
+            { this.onMaximize?.invoke(); other.onMaximize?.invoke() }
+        } else null,
+        onRestore = if (this.onRestore != null || other.onRestore != null) {
+            { this.onRestore?.invoke(); other.onRestore?.invoke() }
+        } else null,
+        onFocusGet = if (this.onFocusGet != null || other.onFocusGet != null) {
+            { this.onFocusGet?.invoke(); other.onFocusGet?.invoke() }
+        } else null,
+        onFocusLost = if (this.onFocusLost != null || other.onFocusLost != null) {
+            { this.onFocusLost?.invoke(); other.onFocusLost?.invoke() }
+        } else null,
+        onResize = if (this.onResize != null || other.onResize != null) {
+            { intSize -> this.onResize?.invoke(intSize); other.onResize?.invoke(intSize) }
+        } else null,
+        onRelocate = if (this.onRelocate != null || other.onRelocate != null) {
+            { intOffset -> this.onRelocate?.invoke(intOffset); other.onRelocate?.invoke(intOffset) }
+        } else null,
     )
 }
